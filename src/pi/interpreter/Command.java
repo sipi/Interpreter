01805db299f47d7dafd3ffd7009b7f1a8d4a1b40
Clément SIPIETER <c.sipieter@gmail.com>
2012-06-14 15:44:26 +0200

    add a manual method on Command Interface

--- src/pi/interpreter/Command.java
+++ src/pi/interpreter/Command.java
@@ -4,4 +4,5 @@ public interface Command
 {
   public String getLabel();
   public int exec(String[] args, Displayer displayer);
+  public String manual();
 };
--- src/pi/interpreter/command/Echo.java
+++ src/pi/interpreter/command/Echo.java
@@ -10,14 +10,20 @@ public class Echo implements Command
   {
     return "echo";
   }
+  
   public int exec(String[] args, Displayer displayer)
   {
     for(int i = 1; i < args.length; ++i)
-      displayer.out.print(args[i]);
+      displayer.out.print(((i>1)? " " : "") + args[i]);
       
     
     displayer.out.println();
     return CommandProcessor.EXIT_SUCCESS;
   }
+  
+  public String manual()
+  {
+    return "";
+  }
 
-}
+};
