7cb30cddb06843af1742d38106206ef1d4444636
Swan Rocher <swan.rocher@gmail.com>
2012-06-16 09:36:23 +0200

    messages

--- src/pi/interpreter/commands/Help.java
+++ src/pi/interpreter/commands/Help.java
@@ -26,27 +26,36 @@ public class Help implements Command {
 	public String syntax() {
 		return SYNTAX_KEYWORD + SYNTAX;
 	}
+
+	public String shortDescription() {
+		return SHORT_DESC;
+	}
+
 	public int exec(String args[], Environment env) {
 		if (args.length != 1)
 			return EXIT_FAILURE;
-		env.out.println("Commands :");
+		int i;
+		String label;
 		Collection<Command> cmds = _processor.getCommands();
 		TreeSet<String> added = new TreeSet<String>();
+		env.out.println("Commands :");
 		for (Command c : cmds) {
-			if (!added.contains(c.getLabel())) {
-				env.out.print("    ");
-				env.out.print(c.getLabel());
-				env.out.print("    ");
+			label = c.getLabel();
+			if (!added.contains(label)) {
+				for (i = 0 ; i < RIGHT_SPACES ; i++)
+					env.out.print(' ');
+				env.out.print(label);
+				for (i = label.length() ; i < LABEL_MAX_SIZE ; i++)
+					env.out.print(' ');
 				env.out.println(c.shortDescription());
-				added.add(c.getLabel());
+				added.add(label);
 			}
 		}
 		return EXIT_SUCCESS;
 	}
 
-	public String shortDescription() {
-		return SHORT_DESC;
-	}
+	private static final int LABEL_MAX_SIZE = 12;
+	private static final int RIGHT_SPACES	= 3;
 
 	private CommandProcessor _processor;
 
